cc78009c706167e167c6df45ba33c185adcedfbd
Duarte <georgelduarte11@gmail.com>
2023-08-28 14:57:30 -0300

    Commit inicial

---
+++ js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productName: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
